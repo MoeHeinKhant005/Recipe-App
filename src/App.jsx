dc84c101bfd2422a7e97c48a01aa963628364605
MoeHeinKhant005 <moeheink578@gmail.com>
2024-04-06 08:07:57 +0630

    Add App.jsx
    Added App.jsx component and its stylings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 className="text-primary font-outfit text-3xl font-bold">Hello World</h1>
+    <div className="App w-full h-auto bg-white flex flex-none flex-col justify-start items-center">
+      
+    </div>
   )
 }
 
